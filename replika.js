a04fd97c3a9c203039f61b8e6838698abe4fac75
gusCreator <lgsc21211@gmail.com>
2024-04-27 08:24:27 -0500

    Añadí la lógica del login del administrador en el servidor

--- replika.js
+++ replika.js
@@ -4,6 +4,7 @@ const path = require('path');
 const express = require('express');
 const WebSocket = require('ws');
 const socket = require('./public/javascript/cliente-ws.js');
+const {resolve} = require('dns');
 
 const app = express();
 
@@ -21,6 +22,29 @@ app.use(express.static('public', {
 
 app.get('/replika', (req, res) => {
   res.sendFile(path.resolve(__dirname, './public/html/user.html'));
+});
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/replika/login', (req, res) => {
+  res.sendFile(path,resolve(__dirname, './public/html/loginAdmin.html'));
+});
+
+
+app.post('/replika/admin', (req, res) => {
+  const {usuario, contraseña} = req.body;
+
+  if(usuario == 'UnicoAdministrador' && contraseña == '12345678'){
+    res.sendFile(path.resolve(__dirname, './public/html/admin.html'));
+
+    const mensaje = "Administrador";
+
+    socket(mensaje);
+
+  }else{
+    res.status(401).end();
+  }
+
 });
 
 app.get('/replika/client/:placa', (req, res) => {
@@ -47,29 +71,37 @@ app.get('/favicon.ico', (req, res) => {
 
 
 const wss = new WebSocket.Server({ server });
-
+let admin = null;
 wss.on('connection', function connection(ws) {
 
-  console.log("Cliente se conectó al WS");
-
-  ws.send("Beinvenido!!");
-
   ws.on('message', function incoming(message) {
-    console.log("Mensaje desde el cliente: ", message.toString());
 
     try {
 
-      const data = JSON.parse(message);
+      if(message == 'Administrador'){
+        admin = ws;
+        console.log("Administrador conectado");
+        return;
+      }
 
-      if(data.accion == 'activarServicio') {
-        // Aquí activaremos el servicio
-        console.log("Activando Servicio");
-        ws.send("Servicio ha sido activado");
-      }else if(data.accion == 'desactivarServicio') {
-        // Aquí desactivamos el servicio
-        console.log("Desactivando servicio");
-      }else{
-        console.warn("Acción no reconocida", data.accion);
+
+        admin.send(message);
+
+        const data = JSON.parse(message);
+
+      //  if(data.accion == 'activarServicio') {
+      //    // Aquí activaremos el servicio
+      //    console.log("Activando Servicio");
+      //    ws.send("Servicio ha sido activado");
+      //  }else if(data.accion == 'desactivarServicio') {
+      //    // Aquí desactivamos el servicio
+      //    console.log("Desactivando servicio");
+      //  }else{
+      //    console.warn("Acción no reconocida", data.accion);
+      //  }
+
+      }else {
+        console.log("Administrador desconectado");
       }
 
     }catch (error) {
@@ -82,4 +114,5 @@ wss.on('connection', function connection(ws) {
   ws.on('close', function close() {
     console.log("Cliente desconectado");
   });
+
 });
