932034cdcd7a227e2da6955d2899eae098863fbe
gusCreator <lgsc21211@gmail.com>
2024-04-29 09:15:17 -0500

    Implementé la renderización de los datos en la página del administrador

--- public/javascript/admin.js
+++ public/javascript/admin.js
@@ -1,3 +1,31 @@
+document.addEventListener('DOMContentLoaded', function() {
+  fetch('/json/cars.json')
+    .then(response => response.json())
+    .then((data) => {
+      const karts = data;
+      const container = document.getElementById('data-karts');
+      let content = '';
+      console.log(karts);
+      for(let i = 0; i < karts.length; i++){
+        content += 
+          `<div style="display:flex;flex-direction:column">
+          <h3>Kart</h3>
+          <h4>Placa: ${karts[i].placa}</h4>
+          <h4>Marca: ${karts[i].marca}</h4>
+          <h4>Modelo: ${karts[i].modelo}</h4>
+          <h4 id="${karts[i].placa}">Disponible: ${karts[i].disponible}</h4>
+          <h4>Característica: ${karts[i].caracteristica}</h4>
+        </div>`;
+      }
+      container.innerHTML = content;
+    })
+    .catch((error) =>  {
+      console.log(error);
+    });
+
+
+});
+
 const mensaje = {
   accion: "conectarAdministrador"
 };
