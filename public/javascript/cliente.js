baff836715809d1286696737a2dffea2de1aae06
gusCreator <lgsc21211@gmail.com>
2024-04-30 10:20:28 -0500

    Próximo paso para los karts. Timeout de los karts durante el servicio

---
+++ public/javascript/cliente.js
@@ -0,0 +1,9 @@
+//contador de tiempo
+//cuando termine el tiempo debe crear un web socket
+// el tiempo se recibe como variable lo debes declarar en service.ejs
+// miras como ejemplo waiting.ejs
+const time = 1000;
+  setTimeout( function() socketCLiente(), time);
+ const mensaje = {
+   accion: "kartTiomeout",
+ 
