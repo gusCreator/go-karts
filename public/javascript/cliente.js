98cd6f82a03500f1481a26a77b5f32584b4f0bfe
ALVARO-QUISPE-UNSA <aquispecondo@unsa.edu.pe>
2024-05-01 16:43:29 -0500

    views/service.ejs

--- public/javascript/cliente.js
+++ public/javascript/cliente.js
@@ -11,16 +11,34 @@
 // Código JavaScript para el contador
 // Aquí puedes utilizar cualquier técnica de contador que desees, por ejemplo:
 console.log('Se cargó exitosamente cliente.js');
-const placa = '<%= placa %>';
-let tiempo = '<%= tiempo %>';
-console.log('Tiempo: ', tiempo, '  placa: ', placa);
-let tiempoRestante = 60; 
 
-function actualizarContador() {
-  document.getElementById('contador_tiempo').innerText = `Tiempo restante: ${tiempoRestante} segundos`;
-  tiempoRestante--; // Decrementar el tiempo restante
+document.addEventListener('DOMContentLoaded', function() {
+  console.log('Placa: ', placa, ' Tiempo: ', tiempo);
+  actualizarContador(tiempo);
+  //se inicia el contador
+});
+
+let iniciarFormulario = ``;
+//funciones
+let actualizarContador = function (tiempo) {
+  let tiempoRestante = parseInt(tiempo);
+  setInterval(function() {
+    document.getElementById('contador_tiempo').innerText = `Tiempo restante: ${tiempoRestante} segundos`;
+    tiempoRestante--;
+    if (tiempoRestante <= 0) {
+      let aviso = "Se acabó el tiempo";
+      document.getElementById('contador_tiempo').innerText = aviso;
+    }
+    tiempoRestante--;
+  }, 1000);
 }
+//se carga
+//se ejecuta el tiempoRestante
+//le dice, que se acabó el tiempo
+//le manda solicitud a la replika
 
 // Actualizar el contador cada segundo (1000 milisegundos)
-setInterval(actualizarContador, 1000);
+
+
+
 
